2ae4a939c29e7747d2bdcf2bb840568123c6396a
wyh <24371063@buaa.edu.cn>
2026-04-11 20:58:59 +0800

    Initial configuration

--- backend/src/modules/video/video.module.ts
+++ backend/src/modules/video/video.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
 import { FollowModule } from '../follow/follow.module';
+import { StorageModule } from '../storage/storage.module';
 import { VideoController } from './video.controller';
 import { MediaService } from './media.service';
 import { VideoService } from './video.service';
 
 @Module({
-  imports: [AuthModule, FollowModule],
+  imports: [AuthModule, FollowModule, StorageModule],
   controllers: [VideoController],
   providers: [VideoService, MediaService],
   exports: [VideoService],
