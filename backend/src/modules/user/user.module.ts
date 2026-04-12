9f39596ed2794ce969a3c1d1b97b4702b331f9e5
wyh <24371063@buaa.edu.cn>
2026-04-12 22:43:49 +0800

    optimize the frontend of user center

--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 
 import { AuthModule } from '../auth/auth.module';
 import { FollowModule } from '../follow/follow.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { StorageModule } from '../storage/storage.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [AuthModule, FollowModule],
+  imports: [AuthModule, FollowModule, StorageModule, PrismaModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
